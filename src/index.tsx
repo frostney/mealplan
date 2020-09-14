89950f2435b16c7348fc8f8e5dff9c6864a38bfa
Johannes Stein <johannesstein@freeze-dev.com>
2020-09-14 22:31:56 +0100

    Add React Native Web

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { AppRegistry } from 'react-native';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+AppRegistry.registerComponent('App', () => App);
+AppRegistry.runApplication('App', { rootTag: document.getElementById('root') });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
